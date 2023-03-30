53d84d7ff98673363980e74e31f7b1c29fed8950
Andrew Lim <andrey.pwn@gmail.com>
2023-03-30 17:13:13 +0300

    - added requests to build.py

--- tools/build.py
+++ tools/build.py
@@ -1,4 +1,5 @@
 from flipper.app import App
+from requests import Session
 
 
 class Main(App):
@@ -13,7 +14,28 @@ class Main(App):
         )
         self.parser.set_defaults(func=self.process)
 
+    @staticmethod
+    def get_bundle_zip(session: Session, uri: str) -> object:
+        print(f"Get from {uri}")
+        response = session.get(uri)
+        return response
+
+    @staticmethod
+    def post_application_build(session: Session, uri: str, app: object) -> None:
+        print(f"Post to {uri}")
+        response = session.post(uri)
+
     def process(self):
+        with Session() as session:
+            # Get bundle from Archivarius through REST
+            bundle = self.get_bundle_zip(session, self.bundle_url)
+
+            # processing bundle
+            # ...
+
+            # Post new application build to Archivarius through REST
+            self.post_application_build(session, self.upload_url, ...)
+
         return 0
 
 
