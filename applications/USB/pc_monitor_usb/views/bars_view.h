ae9901175eb127e9966bde71835a4aba27e2ba52
DonJulve <840710@unizar.es>
2026-01-22 09:27:03 +0100

    Only manifests

--- applications/USB/pc_monitor_usb/views/bars_view.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <gui/gui.h>
-#include "../pc_monitor.h"
-
-void draw_bars_view(Canvas* canvas, void* ctx);
--- applications/USB/pc_monitor_usb/views/connect_view.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <gui/gui.h>
-#include <pc_monitor_usb_icons.h>
-#include "../pc_monitor.h"
-
-void draw_connect_view(Canvas* canvas);
